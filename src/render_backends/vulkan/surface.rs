ebd531b65028089e6e3664072fb0a4506516d4d9
Ilmari Autio <ilmari.autio@gmail.com>
2017-08-10 13:21:28 +0300

    Fix win32 surface code

--- src/render_backends/vulkan/surface.rs
+++ src/render_backends/vulkan/surface.rs
@@ -2,7 +2,7 @@ use ash::vk;
 use ash::Entry;
 use ash::Instance;
 use ash::version::V1_0;
-use ash::extensions::{XlibSurface, Surface};
+use ash::extensions::Surface;
 use winit;
 
 use std::ptr;
@@ -10,9 +10,8 @@ use std::u32;
 use std::error::Error;
 
 pub fn new_loader(entry: &Entry<V1_0>, instance: &Instance<V1_0>) -> Result<Surface, Box<Error>> {
-    let loader = Surface::new(entry, instance).map_err(
-        |_| "Couldn't create surface loader",
-    )?;
+    let loader = Surface::new(entry, instance)
+        .map_err(|_| "Couldn't create surface loader")?;
 
     Ok(loader)
 }
@@ -24,8 +23,11 @@ pub fn new(
     window: &winit::Window,
 ) -> Result<vk::SurfaceKHR, Box<Error>> {
     use winit::os::unix::WindowExt;
+    use ash::extensions::XlibSurface;
 
-    let x11_display = window.get_xlib_display().ok_or("Couldn't get xlib display")?;
+    let x11_display = window
+        .get_xlib_display()
+        .ok_or("Couldn't get xlib display")?;
     let x11_window = window.get_xlib_window().ok_or("Couldn't get xlib window")?;
 
     let x11_create_info = vk::XlibSurfaceCreateInfoKHR {
@@ -36,15 +38,12 @@ pub fn new(
         dpy: x11_display as *mut vk::Display,
     };
 
-    let xlib_surface_loader = XlibSurface::new(entry, instance).map_err(
-        |_| "Unable to load xlib surface",
-    )?;
+    let xlib_surface_loader = XlibSurface::new(entry, instance)
+        .map_err(|_| "Unable to load xlib surface")?;
 
     let surface = unsafe {
-        xlib_surface_loader.create_xlib_surface_khr(
-            &x11_create_info,
-            None,
-        )?
+        xlib_surface_loader
+            .create_xlib_surface_khr(&x11_create_info, None)?
     };
 
     Ok(surface)
@@ -54,29 +53,27 @@ pub fn new(
 pub fn new(
     entry: &Entry<V1_0>,
     instance: &Instance<V1_0>,
-    window: winit::Window,
+    window: &winit::Window,
 ) -> Result<vk::SurfaceKHR, Box<Error>> {
     use winit::os::windows::WindowExt;
+    use ash::extensions::Win32Surface;
 
-    let hwnd = window.get_hwnd().ok_or("Couldn't get window hwnd")?;
+    let hwnd = window.get_hwnd();
 
     let win32_create_info = vk::Win32SurfaceCreateInfoKHR {
         s_type: vk::StructureType::Win32SurfaceCreateInfoKhr,
         p_next: ptr::null(),
         flags: Default::default(),
         hinstance: ptr::null(),
-        hwnd: hwnd as *const (),
+        hwnd: hwnd,
     };
 
-    let win32_surface_loader = Win32Surface::new(entry, instance).map_err(
-        |_| "Unable to load xlib surface",
-    )?;
+    let win32_surface_loader = Win32Surface::new(entry, instance)
+        .map_err(|_| "Unable to load xlib surface")?;
 
     let surface = unsafe {
-        win32_surface_loader.create_win32_surface_khr(
-            &win32_create_info,
-            None,
-        )?
+        win32_surface_loader
+            .create_win32_surface_khr(&win32_create_info, None)?
     };
 
     Ok(surface)
@@ -91,12 +88,10 @@ pub fn new_format(
         .get_physical_device_surface_formats_khr(physical_device, surface.clone())?
         .iter()
         .map(|sfmt| match sfmt.format {
-            vk::Format::Undefined => {
-                vk::SurfaceFormatKHR {
-                    format: vk::Format::B8g8r8Unorm,
-                    color_space: sfmt.color_space,
-                }
-            }
+            vk::Format::Undefined => vk::SurfaceFormatKHR {
+                format: vk::Format::B8g8r8Unorm,
+                color_space: sfmt.color_space,
+            },
             _ => sfmt.clone(),
         })
         .nth(0)
@@ -112,9 +107,8 @@ pub fn new_resolution(
     width: u32,
     height: u32,
 ) -> Result<vk::Extent2D, Box<Error>> {
-    let surface_capabilities =
-        surface_loader
-            .get_physical_device_surface_capabilities_khr(physical_device, surface)?;
+    let surface_capabilities = surface_loader
+        .get_physical_device_surface_capabilities_khr(physical_device, surface)?;
 
     let surface_resolution = match surface_capabilities.current_extent.width {
         u32::MAX => vk::Extent2D { width, height },
